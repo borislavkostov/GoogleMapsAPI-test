021af51bbd013b917f01bf6825e613bfcbbcfb73
borislavkostov <borislavkostov97@gmail.com>
2016-04-17 18:41:25 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Maps Test</title>
+		<?php wp_head(); ?>
+	</head>
+ 
+	<body>
+<iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=High%20College%20of%20Telecommunications%20and%20Posts&key=AIzaSyAymgz0AXCj6ipuLijePEpi72_QcUga23Q" allowfullscreen></iframe>
+    <script src="map.js"> </script>
+
+		
+
+		<?php wp_footer(); ?>
+	</body>
+</html>
